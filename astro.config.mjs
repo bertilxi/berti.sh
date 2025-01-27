ab9f6fb321a50bd46756bc95f111da92495fc883
Fernando Berti <github@berti.sh>
2025-01-27 13:00:10 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  integrations: [react(), tailwind()],
+  adapter: cloudflare(),
+});
