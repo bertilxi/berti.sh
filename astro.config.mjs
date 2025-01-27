b7fb93aac97614133e011f43a2222987694f40e7
Fernando Berti <github@berti.sh>
2025-01-27 13:08:24 -0300

    fix: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ export default defineConfig({
   output: "server",
   integrations: [react(), tailwind()],
   adapter: cloudflare(),
+  redirects: {
+    "/cv":
+      "https://fberti.notion.site/Fernando-Berti-cd746de05b434cd7a19f024af98826b1",
+    "cv-pdf": "https://drive.proton.me/urls/6B3HWC6N0M#G9SeM46Xw8jH",
+  },
   vite: {
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
